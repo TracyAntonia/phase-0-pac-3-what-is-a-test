4a065096a60c334ceb27ec83fb8b1a2f05400e3d
TracyAntonia <aantonjuguna@gmail.com>
2022-11-21 15:20:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+console.log(message)
+
+module.exports = (name, height, message);
+console.log(message)
+
+const name = "Joe";
+const height = 74;
+const message = `${name} is ${height} inches tall`;
+
+module.exports = {name, height, message};
+console.log(message)
